9d582882a256f2e0d815805a163c447467532f46
lancelot <1053876500@qq.com>
2019-11-06 12:48:05 +0800

    add files

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-
+# webDriver 地址
 browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+
 browser.get('http://127.0.0.1:8000')
 assert 'Django' in browser.title
